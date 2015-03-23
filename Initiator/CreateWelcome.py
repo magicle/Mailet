d20101f8602927f982e7d490a7384e2572c8b581
shuai <shuai@shuai-ThinkPad-T400.(none)>
2015-03-23 12:48:37 -0500

    changed number choice to option list in welcome page

---
+++ Initiator/CreateWelcome.py
@@ -0,0 +1,39 @@
+import sys, os
+
+
+
+# read ServerList from ../ServerList file
+
+fd_serverlist = open("../ServerList", "r")
+
+ServerList = list()
+
+for EachLine in fd_serverlist.readlines():
+  ServerList.append(EachLine.strip('\n'))
+
+fd_serverlist.close()
+
+# create welcome page
+
+fd_welcome = open("welcome.html", "r+")
+
+WelcomeLines = fd_welcome.readlines()
+fd_welcome.seek(0)
+for EachLine in WelcomeLines:
+  if "ServerList = " in EachLine:
+    # create ServerList line
+
+    Line = ""
+    for EachServer in ServerList:
+      Line = Line + "\"" + EachServer + "\"" + ","
+    Line = Line.strip(",")
+    Line = "  var ServerList = [" + Line + "];\n"
+    fd_welcome.write(Line)
+  else:
+    fd_welcome.write(EachLine)
+
+fd_welcome.close()
+
+
+
+
